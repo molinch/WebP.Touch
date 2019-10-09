46b4e2f4df12e638d7f08f318e61177e90700b11
Vũ Đức Tuyến <tuyen.vuduc@signkeys.com>
2019-10-09 23:06:10 +0700

    Prepare for fixing issue [#673](https://github.com/xamarin/XamarinComponents/issues/673) in XamarinComponents repository
    - Use libwebp from SDWebImage
    - Update libwebp version to v1.0.3
    - Use carthage for compiling libwebp depedency
    - Use libwebp framework instead of A file
    - Copy dependent frameworks to frameworks for future references
    - Update Nuget package to v1.0.7.3

--- WebP.Mac/WebP.Mac/WebP.linkwith.cs
+++
@@ -1,4 +0,0 @@
-using System;
-using ObjCRuntime;
-
-[assembly: LinkWith ("WebP.a", LinkTarget.i386 | LinkTarget.x86_64, SmartLink = true, ForceLoad = true)]
--- WebP.Touch/WebP.Touch/WebP.linkwith.cs
+++
@@ -1,4 +0,0 @@
-using System;
-using ObjCRuntime;
-
-[assembly: LinkWith ("WebP.a", LinkTarget.Simulator | LinkTarget.Simulator64 | LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Arm64, SmartLink = true, ForceLoad = true)]
